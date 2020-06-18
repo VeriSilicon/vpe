87e2813493fb86b4f70dbc291b7671f6967a0604
Guiyong.zhang <guiyong.zhang@verisilicon.com>
2020-06-18 04:56:19 -0400

    update release script
    Change-Id: Ia50749020d47da712ade25eb4588c63c34e788dd
    Reviewed-on: http://gerrit-spsd.verisilicon.com:8080/1278
    Reviewed-by: Zhang, Guiyong <Guiyong.Zhang@verisilicon.com>

--- release.sh
+++ release.sh
@@ -8,31 +8,31 @@ then
 	exit
 fi
 
-cp Bigsea/software/*.so vpi/sdk_libs/
-cp VC8000D/out/x86_linux_pci/release/*.so vpi/sdk_libs/
-cp VC8000E/software/linux_reference/libh2enc.so vpi/sdk_libs/
-cp common/lib/*.so vpi/sdk_libs/
+cp Bigsea/software/*.so vpe/sdk_libs/
+cp VC8000D/out/x86_linux_pci/release/*.so vpe/sdk_libs/
+cp VC8000E/software/linux_reference/libh2enc.so vpe/sdk_libs/
+cp common/lib/*.so vpe/sdk_libs/
 
-cp Bigsea/software/inc/*.h vpi/sdk_inc/Bigsea/software/inc/
-cp common/inc/*.h vpi/sdk_inc/common/inc/
-cp VC8000E/software/source/vp9/*.h vpi/sdk_inc/VC8000E/software/source/vp9/
-cp VC8000E/software/source/jpeg/*.h vpi/sdk_inc/VC8000E/software/source/jpeg/
-cp VC8000E/software/source/hevc/*.h vpi/sdk_inc/VC8000E/software/source/hevc/
-cp VC8000E/software/source/common/*.h vpi/sdk_inc/VC8000E/software/source/common/
-cp VC8000E/software/linux_reference/ewl/*.h vpi/sdk_inc/VC8000E/software/linux_reference/ewl/
-cp VC8000E/software/inc/*.h vpi/sdk_inc/VC8000E/software/inc/
+cp Bigsea/software/inc/*.h vpe/sdk_inc/Bigsea/software/inc/
+cp common/inc/*.h vpe/sdk_inc/common/inc/
+cp VC8000E/software/source/vp9/*.h vpe/sdk_inc/VC8000E/software/source/vp9/
+cp VC8000E/software/source/jpeg/*.h vpe/sdk_inc/VC8000E/software/source/jpeg/
+cp VC8000E/software/source/hevc/*.h vpe/sdk_inc/VC8000E/software/source/hevc/
+cp VC8000E/software/source/common/*.h vpe/sdk_inc/VC8000E/software/source/common/
+cp VC8000E/software/linux_reference/ewl/*.h vpe/sdk_inc/VC8000E/software/linux_reference/ewl/
+cp VC8000E/software/inc/*.h vpe/sdk_inc/VC8000E/software/inc/
 
-cp VC8000D/software/linux/h264high/*.h vpi/sdk_inc/VC8000D/software/linux/h264high/
-cp VC8000D/software/source/common/*.h vpi/sdk_inc/VC8000D/software/source/common/
-cp VC8000D/software/source/config/*.h vpi/sdk_inc/VC8000D/software/source/config/
-cp VC8000D/software/source/h264high/*.h vpi/sdk_inc/VC8000D/software/source/h264high/
-cp VC8000D/software/source/h264high/legacy/*.h vpi/sdk_inc/VC8000D/software/source/h264high/legacy/
-cp VC8000D/software/source/inc/*.h vpi/sdk_inc/VC8000D/software/source/inc/
-cp VC8000D/software/source/pp/*.h vpi/sdk_inc/VC8000D/software/source/pp/
+cp VC8000D/software/linux/h264high/*.h vpe/sdk_inc/VC8000D/software/linux/h264high/
+cp VC8000D/software/source/common/*.h vpe/sdk_inc/VC8000D/software/source/common/
+cp VC8000D/software/source/config/*.h vpe/sdk_inc/VC8000D/software/source/config/
+cp VC8000D/software/source/h264high/*.h vpe/sdk_inc/VC8000D/software/source/h264high/
+cp VC8000D/software/source/h264high/legacy/*.h vpe/sdk_inc/VC8000D/software/source/h264high/legacy/
+cp VC8000D/software/source/inc/*.h vpe/sdk_inc/VC8000D/software/source/inc/
+cp VC8000D/software/source/pp/*.h vpe/sdk_inc/VC8000D/software/source/pp/
 
-rm vpi/drivers/ -rf
+sudo rm vpe/drivers/ -rf
 cd drivers
 find . -name *.o | xargs rm
 cd -
-cp drivers/ vpi/ -rf
+cp drivers/ vpe/ -rf
 

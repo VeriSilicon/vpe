be19b5d87bb834fd2573b460c08ec88526d8f0f6
Guiyong.zhang <guiyong.zhang@verisilicon.com>
2020-09-03 01:54:09 -0400

    add more compiling opition
    Change-Id: I50532a3c3eeab2a193387971132374bfe22d6f1e
    Reviewed-on: http://gerrit-spsd.verisilicon.com:8080/1998
    Reviewed-by: Wang, Qin <Qin.Wang@verisilicon.com>
    Reviewed-by: Zhang, Guiyong <Guiyong.Zhang@verisilicon.com>

--- build_vpe.sh
+++ build_vpe.sh
@@ -1,30 +1,75 @@
 #!/bin/sh
 
 clean_option=n
-dbg=n
+debug=n
+mini_ffmpeg=n
+
+if [ "$1" == "mini_ffmpeg" ] || [ "$2" == "mini_ffmpeg" ] || [ "$3" == "mini_ffmpeg" ]
+	then
+	mini_ffmpeg=y
+fi
+
+if [ "$1" == "debug" ] || [ "$2" == "debug" ] || [ "$3" == "debug" ]
+	then
+	debug=y
+fi
+
+if [ "$1" == "clean" ]
+	then
+	clean_option=y
+fi
+
+echo "Compiling opition: debug=$debug, mini_ffmpeg=$mini_ffmpeg, clean_option=$clean_option"
+
+if [ "${clean_option}" == "y" ]
+then
+	cd vpe
+	make clean
+	cd ../ffmpeg
+	make clean
+	exit 0
+fi
 
 cd vpe
-	if [ "${clean_option}" == "y" ]
-	then
-		make clean
-	fi
-	make DEBUG=${dbg}
+	make DEBUG=${debug}
 	sudo make install
 cd -
 
 cd ffmpeg
-	if [ "${dbg}" == "y" ]
+	if [ "${debug}" == "y" ] && [ "${mini_ffmpeg}" == "n" ]
 	then
 		./configure --enable-vpe --extra-ldflags="-L/lib/vpe" --extra-libs="-lvpi" \
-			--disable-optimizations \
-			--disable-asm  \
-			--disable-stripping
+		--disable-optimizations \
+		--disable-asm  \
+		--disable-stripping
+	elif [ "${dbg}" == "y" ] && [ "${mini_ffmpeg}" == "y" ]
+	then
+		./configure --disable-sdl2 --enable-vpe --extra-ldflags="-L/lib/vpe" --extra-libs="-lvpi" \
+		--disable-optimizations \
+		--disable-asm  \
+		--disable-stripping \
+		--disable-sdl2 \
+		--disable-libxcb \
+		--disable-libxcb-shm \
+		--disable-libxcb-xfixes \
+		--disable-libxcb-shape \
+		--disable-xlib \
+		--disable-libmfx \
+		--disable-vaapi
+	elif [ "${debug}" == "n" ] && [ "${mini_ffmpeg}" == "y" ]
+	then
+		./configure --enable-vpe  --extra-ldflags="-L/lib/vpe" --extra-libs="-lvpi" \
+		--disable-sdl2 \
+		--disable-libxcb \
+		--disable-libxcb-shm \
+		--disable-libxcb-xfixes \
+		--disable-libxcb-shape \
+		--disable-xlib \
+		--disable-libmfx \
+		--disable-vaapi
 	else
-		./configure --enable-vpe  --extra-ldflags="-L/lib/vpe" --extra-libs="-lvpi"
+    ./configure --enable-vpe  --extra-ldflags="-L/lib/vpe" --extra-libs="-lvpi"
 	fi
-	if [ "${clean_option}" == "y" ]
-        then
-			make clean
-        fi
-	make -j 32
+	make -j8
+cd -
 

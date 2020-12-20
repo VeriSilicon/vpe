445375cda8aa167a81c3cfc7d6d08124cada5883
Guiyong.zhang <guiyong.zhang@verisilicon.com>
2020-12-20 06:35:12 -0500

    add VPE cross compiling scripts
    Change-Id: Ia7d09d13ed5dbf3002bf5e942e2c59b5e170f437
    Reviewed-on: http://gerrit-spsd.verisilicon.com:8080/3242
    Reviewed-by: Zhang, Guiyong <Guiyong.Zhang@verisilicon.com>

--- build/build_vpe.sh
+++ build/build_vpe.sh
@@ -1,78 +1,97 @@
 #!/bin/sh
 
-clean_option=n
+if [ -x "config.mk" ]; then
+source ./config.mk
+fi
+
 debug=n
-mini_ffmpeg=n
+cross=n
+host_arch=`uname -m`
+arch=$ARCH
 
-if [ "$1" == "mini_ffmpeg" ] || [ "$2" == "mini_ffmpeg" ] || [ "$3" == "mini_ffmpeg" ]
-	then
-	mini_ffmpeg=y
+if [ "$1" == "x86_64" ]; then
+arch=x86_64
 fi
 
-if [ "$1" == "debug" ] || [ "$2" == "debug" ] || [ "$3" == "debug" ]
-	then
-	debug=y
+if [ -z "$ARCH" ]; then
+arch=x86_64
 fi
 
-if [ "$1" == "clean" ]
-	then
-	clean_option=y
+if [ "$1" == "aarch64" ]; then
+arch=arm64
 fi
 
-echo "Compiling opition: debug=$debug, mini_ffmpeg=$mini_ffmpeg, clean_option=$clean_option"
+if [ "$arch" == "aarch64" ]; then
+arch=arm64
+fi
 
-if [ "${clean_option}" == "y" ]
-then
-	cd ../
-	make clean
-	cd ../ffmpeg
-	make clean
-	exit 0
+if [[ "$arch" != "$host_arch" ]]; then
+echo "Target arch is $arch, host arch is $host_arch, so enable cross compiling"
+cross=y
+fi
+
+cmd="./configure --disable-vaapi --enable-vpe "
+
+if [ "$1" == "clean" ]; then
+        cd ../
+        make clean
+        cd ../ffmpeg
+        make clean
+        exit 0
 fi
 
 cd ../
-	make DEBUG=${debug}
-	sudo make install
-cd -
 
-cd ../../ffmpeg
-	if [ "${debug}" == "y" ] && [ "${mini_ffmpeg}" == "n" ]
-	then
-		./configure --enable-vpe --extra-ldflags="-L/usr/lib/vpe" --extra-libs="-lvpi" \
-		--disable-optimizations \
-		--disable-asm  \
-		--disable-stripping \
-		--disable-vaapi
-	elif [ "${dbg}" == "y" ] && [ "${mini_ffmpeg}" == "y" ]
-	then
-		./configure --disable-sdl2 --enable-vpe --extra-ldflags="-L/usr/lib/vpe" --extra-libs="-lvpi" \
-		--disable-optimizations \
-		--disable-asm  \
-		--disable-stripping \
-		--disable-sdl2 \
-		--disable-libxcb \
-		--disable-libxcb-shm \
-		--disable-libxcb-xfixes \
-		--disable-libxcb-shape \
-		--disable-xlib \
-		--disable-libmfx \
-		--disable-vaapi
-	elif [ "${debug}" == "n" ] && [ "${mini_ffmpeg}" == "y" ]
-	then
-		./configure --enable-vpe  --extra-ldflags="-L/usr/lib/vpe" --extra-libs="-lvpi" \
-		--disable-sdl2 \
-		--disable-libxcb \
-		--disable-libxcb-shm \
-		--disable-libxcb-xfixes \
-		--disable-libxcb-shape \
-		--disable-xlib \
-		--disable-libmfx \
-		--disable-vaapi
-	else
-		./configure --enable-vpe  --extra-ldflags="-L/usr/lib/vpe" --extra-libs="-lvpi" \
-		--disable-vaapi
+make DEBUG=${debug} ARCH=$arch
+if [ $? != 0 ]; then echo "build VPE error";exit 1; fi
 
-	fi
+if [ "${cross}" == "y" ]; then
+	export PKG_CONFIG_PATH=`pwd`:$PKG_CONFIG_PATH
+	echo $PKG_CONFIG_PATH
+        echo "Name: libvpi" > "libvpi.pc"
+	echo "Description: VSI platform interface lib" >> "libvpi.pc"
+	echo "Version: 1.0" >> "libvpi.pc"
+	echo "Cflags: -I"`pwd`"/vpi_inc/" >> "libvpi.pc"
+	echo "Libs: -L"`pwd`"/sdk_libs/"${arch}"/" -L"`pwd`""/vpi -lvpi" >> "libvpi.pc"
+	mkdir vpi_inc/vpe -p
+	cp vpi/inc/*.h vpi_inc/vpe/
+else
+        sudo make install ARCH=$arch
+fi
+
+cd ../ffmpeg
+
+if [ "${debug}" == "y" ]; then
+	cmd=$cmd"--disable-optimizations --disable-asm --disable-stripping "
+fi
+
+if [ "${cross}" == "n" ]; then
+	cmd=$cmd"--extra-ldflags="-L/usr/lib/vpe" --extra-libs="-lvpi" "
+else
+	cmd=$cmd\
+"--pkg-config=true "\
+"--disable-shared "\
+"--enable-static "\
+"--cross-prefix=$CROSS_COMPILE "\
+"--enable-cross-compile "\
+"--arch=${arch} "\
+"--extra-cflags=-I../vpe/vpi_inc "\
+"--extra-ldflags=\"-L./../vpe/sdk_libs/${arch} -L./../vpe/vpi/\" "\
+"--extra-libs=\"-lvpi -lcwl -ldwlg2 -lenc -lg2common -lg2h264 -lg2hevc -lg2vp9 -lh2enc -lhal -lpp -lsyslog\" "\
+"--target-os=linux "\
+"--sysroiot=$SDKTARGETSYSROOT "
+fi
+	echo "command is $cmd"
+	echo $cmd > config.sh
+	chmod 777 config.sh
+	./config.sh
+	if [ $? != 0 ]; then echo "FFmpeg configureation error";exit 1; fi
+
+	rm config.sh
 	make -j8
-cd -
+	if [ $? != 0 ]; then echo "build FFmpeg error";exit 1; f
 
+if [ "${cross}" == "y" ]; then
+	rm ../vpe/vpi_inc -rf
+	rm ../vpe/libvpi.pc
+fi
--- drivers/build_drivers.sh
+++ drivers/build_drivers.sh
@@ -2,6 +2,7 @@
 
 cd transcoder-pcie
 make clean all
+if [ $? != 0 ]; then echo "build driver error";exit 1; fi
 cp transcoder.h ../
 cd -
 
